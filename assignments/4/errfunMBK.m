49ecfa84d047cccacb048c64c17e01225a0cd14c
Oliver Lee <oliverzlee@gmail.com>
2014-12-05 14:25:56 +0100

    A4.2 Add option to errfunMBK.m to change error calculation
    Add option to use frequency and coherence weighing in error function.

--- assignments/4/errfunMBK.m
+++ assignments/4/errfunMBK.m
@@ -1,16 +1,18 @@
-function [e,Hef] = errfunMBK(P, frf, fvec,mCoh)
-   
-    M    = P(1);
-    B    = P(2);
-    K    = P(3);
-           
-    s=2*pi*1j*fvec;
-    s2=(-4*pi^2*fvec.^2);
-
-    Hef=1./(M*s2+B.*s+K);
-
-    e=abs(log(frf./Hef)); % 
+function [e, Hef] = errfunMBK(P, frf, fvec, mCoh, est)
+if nargin < 5
+    est = false;
 end
 
+PP = num2cell(P);
+[M, B, K] = deal(PP{:});
 
+s = 2*pi*1j*fvec;
+s2 = -4*pi^2*fvec.^2;
 
+Hef = 1./(M*s2 + B.*s + K);
+
+if ~est
+    e = abs(log(frf./Hef));
+else
+    e = abs(log(frf./Hef)) .* mCoh .* sqrt(1./fvec);
+end
